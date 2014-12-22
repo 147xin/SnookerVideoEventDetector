d51c9fa68bbc22e8e24f10fbc83c9aaf3ca29f1b
hix <yef147@gmail.com>
2014-12-22 11:23:12 +0800

    Initial Commit

---
+++ SnookerVideoEventDetector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SnookerVideoEventDetector
+//
+//  Created by Yixin Huang on 14/12/22.
+//  Copyright (c) 2014年 Yixin Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
